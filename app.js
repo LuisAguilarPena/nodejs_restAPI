b36f11bee2af1b8741d95775dac79fa07f23a95b
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-04-17 10:31:43 -0500

    "successfully implemented the root route"

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
 const express = require('express');
 // 2. assign a variable to a new instance of express
 const app = express()
+// 4. app.get and / to especify the root, cb funct to especify how you wanna handle the request with 2 parameters the actual request that comes from your browser and the response you want to give to this particular get request 
+app.get('/', (req, res) => {
+  console.log("Responding to root route");
+  res.send("'Hello from root :3")
+})
 // 3. listen to an especific port and espcify a cb funct
 app.listen(3003, () => {
   console.log('Server listening on port 3003 :D');
 })
-// if you run node app.js and go to localhost:3003 you will get a Cannot GET /
+// if you run node app.js and go to localhost:3003 you will get a Cannot GET / that is because we have not especified any root route, do that above the listen
