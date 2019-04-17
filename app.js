9a24b7a587468682f95758dd60284d63981fad81
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-04-17 14:23:59 -0500

    route /user/:id to try and fetch data from mysql

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express()
 // 9. added morgan to get detailed info for get reqs, so everytime I refresh a route in the browser some info will be given to use in the terminal
 const morgan = require('morgan')
 app.use(morgan('short')) // type of req, route, time
-app.use(morgan('combined')) // more detailed info
-// 4. app.get and / to especify the root, cb funct to especify how you wanna handle the request with 2 parameters the actual request that comes from your browser and the response you want to give to this particular get request 
+//app.use(morgan('combined')) // more detailed info
+// 4. app.get and / to specify the root, cb funct to specify how you wanna handle the request with 2 parameters the actual request that comes from your browser and the response you want to give to this particular get request 
 app.get('/', (req, res) => { // so this is localhost:3003
   console.log('Responding to root route');
   res.send("'Hello from root :3")
@@ -17,9 +17,12 @@ app.get('/testRoute1', (req, res) => {
   res.send('Nodemon autoupdates when I save this file, I still need to refresh tho, only updates server so I dont have to run server again')
 })
 // 7. create another route to test, now it will return some json
-app.get('/testRoute2', (req, res) => {
+app.get('/users', (req, res) => {
   let user1 = {firstName: 'Giannis', lastName: 'Antetokounmpo'}
-  res.json(user1)
+  let user2 = {firstName: 'Scottie', lastName: 'Pippen'}
+  let user3 = {firstName: 'Dennis', lastName: 'Rodman'}
+  // res.json(user1)
+  res.json([user1, user2, user3])
 })
 // 8. create another route to test, now it will return some json
 app.get('/bullsRosterTest', (req, res) => {
@@ -29,6 +32,12 @@ app.get('/bullsRosterTest', (req, res) => {
   let x = user1.tasks[0]  
   res.json([user1, user2, user3, x])
 })
+// 10. specify a new route that is going to be connected to our database, the route is going to catch the id using the wildcard syntax
+app.get('/user/:id', (req, res) => {
+  console.log(`Fetching user with id: ${req.params.id}`);
+  res.end()
+  //console.log(req, '(-_-)');
+}) 
 // 3. listen to an especific port and espcify a cb funct
 app.listen(3003, () => {
   console.log('Server listening on port 3003 :D');
