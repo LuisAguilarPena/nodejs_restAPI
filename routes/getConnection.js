4b085b0aed909e1e11b190131ffd27e321ff9e30
LuisAguilarPena <46686358+LuisAguilarPena@users.noreply.github.com>
2019-04-19 12:50:54 -0500

    ClearDB instructions

--- routes/getConnection.js
+++ routes/getConnection.js
@@ -1,6 +1,12 @@
 //Details to connect to mysql
 const mysql = require('mysql')
 // create a connection pool in order to avoid fail connections to mysql
+// in order to make it work deployed ill use ClearDB
+// $ heroku addons:create cleardb:ignite
+// $ heroku config
+// get mysql info from the CLEARDB DATABSE URL
+// connect to cleardb through mysql and access db given an create table
+// $ mysql --host=host --user=user --password=pass (edited) 
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
